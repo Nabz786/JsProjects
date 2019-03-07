c361afc950b9c157b7d54801a3bf8e2edc90128b
Nabeel V <nabvali98@gmail.com>
2019-03-07 12:46:41 -0500

    Implemeted game loop without user input validation

--- othello/main.js
+++ othello/main.js
@@ -15,7 +15,7 @@ const prompt = require('prompt-sync')();
  * @param file - The full filename path we want to save to.
  * @param contents - The object we wish to save as a JSON file.
  */
-function saveFile(file, contents){
+function saveFile(file, contents) {
 	let fs = require('fs');
 	fs.writeFileSync(file, JSON.stringify(contents));
 }
@@ -26,31 +26,84 @@ function saveFile(file, contents){
  * @param file - The full filename path we wish to load an object from.
  * @return contents - The object converted from JSON.
  */
-function loadFile(file){
-
+function loadFile(file) {
+	let boardToLoad = JSON.parse(fs.readFileSync(file, 'utf8'));
+	return boardToLoad;
 }
 
 /**
  * Driver function.  "main" method, if you will.
  */
-function start(){
- 	// Local variables
-	let height = prompt('What height for your board? ');
-	let width = prompt('What width for your board? ');
+function start() {
+	// // Local variables
+	//  let height = prompt('What height for your board? ');
+	//  let width = prompt('What width for your board? ');
 
-	// SYNCHRONOUSLY read from keyboard
-	console.log('Creating a board with size ' + height + ' x ' + width + '.');
-	// Create new board object
-	let myBoard = new board(height, width);
 
-	// Print board
+	// // SYNCHRONOUSLY read from keyboard
+	//  console.log('Creating a board with size ' + height + ' x ' + width + '.');
+	// // Create new board object
+	// let myBoard = new board(height, width);
+
+	// // Print board
+	// myBoard.initBoard();
+	// myBoard.printBoard();
+
+	// // Loop, asking user input, calling appropriate functions.
+
+	// // Save board example code.
+	// saveFile("test.json", myBoard);
+
+	let p1Disc = prompt("Enter Your Disc Color B or W: ");
+	var p2Disc = p1Disc;
+	var turn = 1;
+	if (p1Disc === "B") {
+		var p2Disc = "W";
+	}
+
+	console.log();
+	console.log("<<<<<Welcome To othello!>>>>>");
+	console.log("Player 1: " + p1Disc + " Player 2: " + p2Disc);
+	console.log("Player " + turn + " will start the game!");
+	let myBoard = new board(8, 8);
 	myBoard.initBoard();
-	myBoard.printBoard();
 
-	// Loop, asking user input, calling appropriate functions.
+	var row, col;
+	while (!myBoard.isGameOver(myBoard.board)) {
+		myBoard.printBoard();
+		if (!myBoard.isValidMoveAvailable((turn === 1 ? p1Disc : p2Disc))) {
+			console.log("No Valid moves available for player " + turn + " you will lose your turn");
+		} else {
+			do {
+				console.log("To Quit, Enter Q in both prompts");
+				var row = prompt("Player: " + turn + " ,Enter the location row to place your disc: ");
+				var col = prompt("Player: " + turn + " ,Enter the location col to place your disc: ");
+				if (row < 1 || row > 8 || col < 1 || col > 8) {
+					console.log("Sorry, invalid input. Try again");
+					continue;
+				} else if (row === "Q") {
+					process.exit(1);
+				}
+				row--;
+				col--;
+				if (!myBoard.isValid(row, col, (turn === 1 ? p1Disc : p2Disc))) {
+					console.log("Sorry, Invalid move, try again");
+					continue;
+				}
+				break;
+			} while (true) {
+				myBoard.placeDiskAt(row, col, (turn === 1 ? p1Disc : p2Disc));
+			}
+			turn = turn === 1 ? 2 : 1;
+		}
+	}
 
-	// Save board example code.
-	saveFile("test.json", myBoard);
+
+
+
+
+
+
 }
 
 console.clear();

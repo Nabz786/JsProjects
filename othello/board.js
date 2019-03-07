895328c22498febc5cd18934b4920ed3d9d56d6f
Nabeel V <nabvali98@gmail.com>
2019-03-07 12:46:23 -0500

    Implemented all required functions

--- othello/board.js
+++ othello/board.js
@@ -4,29 +4,33 @@
  */
 
 module.exports = class Board {
+
+
+
 	/**
 	 * Construct the object with required state
 	 */
-	constructor(height, width){
+	constructor(height, width) {
 		this.height = height;
 		this.width = width;
 		this.board = [];
-		for(let i=0; i<this.height; ++i){
+		for (let i = 0; i < this.height; ++i) {
 			let tmp = [];
-			for(let j=0; j<this.width; ++j){
+			for (let j = 0; j < this.width; ++j) {
 				tmp.push(-1);
 			}
 			this.board.push(tmp);
 		}
 	}
 
+
 	/**
 	 * Print a representation of the board to the terminal.
 	 */
-	printBoard(){
-		for(let i=0; i<this.height; ++i){
-			for(let j=0; j<this.width; ++j){
-				if(this.board[i][j] == -1){
+	printBoard() {
+		for (let i = 0; i < this.height; ++i) {
+			for (let j = 0; j < this.width; ++j) {
+				if (this.board[i][j] == -1) {
 					process.stdout.write('.\t')
 				} else {
 					process.stdout.write(this.board[i][j] + "\t")
@@ -55,9 +59,140 @@ module.exports = class Board {
 	 * @param disc A character for the disc color.
 	 * @return A boolean indicating whether the move is valid.
 	 */
-	
-	isValid(row, col, disc){
 
+	isValid(row, col, disc) {
+
+		// console.log(disc);
+		// console.log(row);
+		var SIZE = 8;
+		var EMPTY = -1;
+
+		if (this.board[row][col] != EMPTY) {
+			return false;
+		}
+
+		//Check up
+		for (let i = row - 1; i > -1; i--) {
+			if (this.board[i][col] != disc && this.board[i][col] != EMPTY) {
+				if (this.board[i - 1][col] == disc) {
+					console.log("\nU\n");
+					return true;
+				}
+				continue;
+			} else if (this.board[i][col] == EMPTY || row == 0) {
+				break;
+			}
+		}
+
+
+		//Check Down
+		for (let i = row + 1; i < 8; i++) {
+			if (this.board[i][col] != disc && this.board[i][col] != EMPTY) {
+				if (this.board[i + 1][col] == disc) {
+					console.log("\nD\n");
+					return true;
+				}
+			} else if (this.board[i][col] == EMPTY || row == SIZE - 1) {
+				break;
+			}
+		}
+
+		//Check Left
+		for (let i = col - 1; i > -1; i--) {
+			if (this.board[row][i] != disc && this.board[row][i] != EMPTY) {
+				if (this.board[row][i - 1] == disc) {
+					console.log("\nL\n");
+					return true;
+				}
+				continue;
+			} else if (this.board[row][i] == EMPTY || col == 0) {
+				break;
+			}
+		}
+
+		//Check Right
+		for (let i = col + 1; i < SIZE; i++) {
+			if (this.board[row][i] != disc && this.board[row][i] != EMPTY) {
+				if (this.board[row][i + 1] == disc) {
+					console.log("\nR\n");
+					return true;
+				}
+				continue;
+			} else if (this.board[row][i] == EMPTY || col == SIZE - 1) {
+				break;
+			}
+		}
+
+
+		//Temp value used to iterate through columns 
+		//When moving diagonally
+		var colIter = 0;
+
+		//Check Diag-Up-Left
+		colIter = col + 1;
+		for (let i = row + 1; i < SIZE; i++) {
+			if (colIter >= SIZE) {
+				break;
+			}
+			if (this.board[i][colIter] != disc && this.board[i][colIter] != EMPTY
+				&& this.board[i + 1][colIter + 1] == disc) {
+				console.log("\nU-L\n");
+				return true;
+			} else if (this.board[i][colIter] == EMPTY) {
+				break;
+			}
+			colIter++;
+		}
+
+		//Check Diag Up-right
+		colIter = col - 1;
+		for (let i = row + 1; i < SIZE; i++) {
+			if (colIter < 0) {
+				break;
+			}
+			if (this.board[i][colIter] != disc && this.board[i][colIter] != EMPTY
+				&& this.board[i + 1][colIter - 1] == disc) {
+				console.log("\nU-R\n");
+				return true;
+			} else if (this.board[i][colIter] == EMPTY) {
+				break;
+			}
+			colIter--;
+		}
+
+		//Cjeck Diag bottom-right
+		colIter = col + 1;
+		for (let i = row - 1; i > -1; i--) {
+			if (colIter >= SIZE) {
+				break;
+			}
+			if (this.board[i][colIter] != disc && this.board[i][colIter] != EMPTY
+				&& this.board[i - 1][colIter + 1] == disc) {
+				console.log("\nD-R\n");
+				return true;
+			} else if (this.board[i][colIter] == EMPTY) {
+				break;
+			}
+			colIter++;
+		}
+
+		//Check Diag-Bottom-Left
+		colIter = col - 1;
+		for (let i = row - 1; i > -1; i--) {
+			if (colIter < 0) {
+				break;
+			}
+			if (this.board[i][colIter] != disc && this.board[i][colIter] != EMPTY
+				&& this.board[i - 1][colIter - 1] == disc) {
+				console.log("\nD-L\n");
+				return true;
+			} else if (this.board[i][colIter] == EMPTY) {
+				break;
+			}
+			colIter--;
+		}
+
+		return false;
 	}
 
 	/**
@@ -66,8 +201,188 @@ module.exports = class Board {
 	 * @param col An integer number for column.
 	 * @param disc A character standing for disc color.
 	 */
-	placeDiskAt(row, col, disc){
+	placeDiskAt(row, col, disc) {
 
+		this.board[row][col] = disc;
+
+		var numWrite = 0;
+		var colIter = 0;
+		var SIZE = 8;
+		var EMPTY = -1;
+
+		//Check move up
+		for (let i = row + 1; i < SIZE; i++) {
+			numWrite++;
+			if (this.board[i][col] != disc && this.board[i][col] != EMPTY &&
+				this.board[i + 1][col] == disc) {
+				console.log("\nUp\n");
+				for (let j = row + 1; j < SIZE; j++) {
+					if (numWrite > 0) {
+						this.board[j][col] = disc;
+						numWrite--;
+					}
+				}
+			} else if (this.board[i][col] == EMPTY) {
+				break;
+			}
+		}
+
+		numWrite = 0;
+		//Check Move down
+		for (let i = row - 1; i > -1; i--) {
+			numWrite++;
+			if (this.board[i][col] != disc && this.board[i][col] != EMPTY &&
+				this.board[i - 1][col] == disc) {
+				console.log("\nDown\n");
+				console.log("\n%d\n", numWrite);
+				for (let j = row - 1; j > -1; j--) {
+					if (numWrite > 0) {
+						this.board[j][col] = disc;
+						numWrite--;
+					}
+				}
+			} else if (this.board[i][col] == EMPTY) {
+				break;
+			}
+		}
+
+		numWrite = 0;
+		//Check Move right
+		for (let i = col - 1; i > -1; i--) {
+			numWrite++;
+			if (this.board[row][i] != disc && this.board[row][i] != EMPTY &&
+				this.board[row][i - 1] == disc) {
+				console.log("\nRight\n");
+				console.log("\n%d\n", numWrite);
+				for (let j = col - 1; j > -1; j--) {
+					if (numWrite > 0) {
+						this.board[row][j] = disc;
+						numWrite--;
+					}
+				}
+			} else if (this.board[row][i] == EMPTY) {
+				break;
+			}
+		}
+
+		numWrite = 0;
+		//Check Move left
+		for (let i = col + 1; i < SIZE; i++) {
+			numWrite++;
+			if (this.board[row][i] != disc && this.board[row][i] != EMPTY &&
+				this.board[row][i + 1] == disc) {
+				console.log("\nLeft\n");
+				console.log("\n%d\n", numWrite);
+				for (let j = col + 1; j < SIZE; j++) {
+					if (numWrite > 0) {
+						this.board[row][j] = disc;
+						numWrite--;
+					}
+				}
+			} else if (this.board[row][i] == EMPTY) {
+				break;
+			}
+		}
+
+		numWrite = 0;
+		colIter = col - 1;
+		//Check Up Right
+		for (let i = row + 1; i < SIZE; i++) {
+			if (colIter < 0) {
+				break;
+			}
+			numWrite++;
+			if (this.board[i][colIter] != disc && this.board[i][colIter] != EMPTY &&
+				this.board[i + 1][colIter - 1] == disc) {
+				console.log("\nUp-Right\n");
+				let colTemp = col - 1;
+				for (let j = row + 1; j < SIZE; j++) {
+					if (numWrite > 0) {
+						this.board[j][colTemp] = disc;
+						colTemp--;
+						numWrite--;
+					}
+				}
+			} else if (this.board[i][colIter] == EMPTY) {
+				break;
+			}
+			colIter--;
+		}
+
+		numWrite = 0;
+		colIter = col + 1;
+		//Check Up Left
+		for (let i = row + 1; i < SIZE; i++) {
+			if (colIter >= SIZE) {
+				break;
+			}
+			numWrite++;
+			if (this.board[i][colIter] != disc && this.board[i][colIter] != EMPTY &&
+				this.board[i + 1][colIter + 1] == disc) {
+				console.log("\nUp-Left\n");
+				let colTemp = col + 1;
+				for (let j = row + 1; j < SIZE; j++) {
+					if (numWrite > 0) {
+						this.board[j][colTemp] = disc;
+						colTemp++;
+						numWrite--;
+					}
+				}
+			} else if (this.board[i][colIter] == EMPTY) {
+				break;
+			}
+			colIter++;
+		}
+
+		numWrite = 0;
+		colIter = col + 1;
+		//Check down-left
+		for (let i = row - 1; i > -1; i--) {
+			if (colIter >= SIZE) {
+				break;
+			}
+			numWrite++;
+			if (this.board[i][colIter] != disc && this.board[i][colIter] != EMPTY
+				&& this.board[i - 1][colIter + 1] == disc) {
+				console.log("\nDown-left\n");
+				let colTemp = col + 1;
+				for (let j = row - 1; j > -1; j--) {
+					if (numWrite > 0) {
+						this.board[j][colTemp] = disc;
+						colTemp++;
+						numWrite--;
+					}
+				}
+			} else if (this.board[i][colIter] == EMPTY) {
+				break;
+			}
+			colIter++;
+		}
+
+		numWrite = 0;
+		colIter = col - 1;
+		//Check down-right
+		for (let i = row - 1; i > -1; i--) {
+			if (colIter < 0) {
+				break;
+			}
+			numWrite++;
+			if (this.board[i][colIter] != disc && this.board[i][colIter] != EMPTY
+				&& this.board[i - 1][colIter - 1] == disc) {
+				console.log("\nDown-right\n");
+				let colTemp = col - 1;
+				for (let j = row - 1; j > -1; j--) {
+					if (numWrite > 0) {
+						this.board[j][colTemp] = disc;
+						colTemp--;
+						numWrite--;
+					}
+				}
+			} else if (this.board[i][colIter] == EMPTY) {
+				break;
+			}
+			colIter--;
+		}
 	}
 
 	/**
@@ -76,21 +391,33 @@ module.exports = class Board {
 	 * @return bool A boolean telling the user whether there are
 	 *	 	valid moves availabe for that disc.
 	 */
-	isValidMoveAvailable(disc){
+	isValidMoveAvailable(disc) {
+		console.log(disc);
 
+		var SIZE = 8;
+
+		for (let i = 0; i < SIZE; i++) {
+			for (let j = 0; j < SIZE; j++) {
+				if (this.isValid(i, j, disc)) {
+					return true;
+				}
+			}
+		}
+
+		return false;
 	}
 
 	/**
 	 * isBoardFull
 	 * @return boolean Whether or not the board is full.
 	 */
-	isBoardFull(){
-		for(let i = 0; i < this.height; ++i) {
-				for(let j = 0; j < this.width; ++j) {
-					if(this.board[i][j] == -1) {
-						return false;
-					}
+	isBoardFull() {
+		for (let i = 0; i < this.height; ++i) {
+			for (let j = 0; j < this.width; ++j) {
+				if (this.board[i][j] == -1) {
+					return false;
 				}
+			}
 		}
 		return true;
 	}
@@ -99,8 +426,13 @@ module.exports = class Board {
 	 * isGameOver
 	 * @return bool Whether or not the game is over.
 	 */
-	isGameOver(){
+	isGameOver() {
+		if (this.isBoardFull(this.board) || !this.isValidMoveAvailable("W") ||
+			!this.isValidMoveAvailable("B")) {
+			return true;
+		}
 
+		return false;
 	}
 
 	/**
@@ -108,8 +440,30 @@ module.exports = class Board {
 	 * @return char Which player has won.  Return null if
 	 * 		a tie exists.
 	 */
-	checkWinner(){
+	checkWinner() {
+		let bCount = 0;
+		let wCount = 0;
 
+		let SIZE = 8;
+
+		for (let i = 0; i < SIZE; i++) {
+			for (let j = 0; j < SIZE; j++) {
+				if (this.board[i][j] == 'W') {
+					wCount++;
+				} else {
+					bCount++;
+				}
+			}
+		}
+
+		if (wCount > bCount) {
+			return "W";
+		} else if (bCount > wCount) {
+			return "B";
+		} else {
+			return -1;
+		}
+		return -1;
 	}
 }
 
